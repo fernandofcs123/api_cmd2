f563b947f1bfbd89417e9edee25e6306f4993572
aluno <aluno@UNIFEOB-ACAD.LOCAL>
2024-11-14 22:19:35 -0300

    modificação login controller v2

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,7 +6,7 @@ const AdministradoresModel = require('../models/administradoresModel')
 
 class LoginController {
 
-    // async login(req,res){                                                    CASEIRO
+    // async login(req,res){                                                    
     //     let {email, senha} = req.body
     //     const administrador = new AdministradoresModel({email, senha})
     //     let adm = await administradoresRepo.buscar_email(administrador)
@@ -18,7 +18,7 @@ class LoginController {
     //         let isPassword = await bcrypt.compare(senha,adm.values.senha)
 
     //         if(!isPassword){
-    //             res.status(404).json({success: user.status, message: "Senha incorreta"})
+    //             res.status(404).json({success: adm.status, message: "Senha incorreta"})
     //         } else {
     //             let token = jwt.sign({email: adm.values.email, role: adm.values.role}, process.env.JWT_SECRET, {expiresIn: 600})
 
